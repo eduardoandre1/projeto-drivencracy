b25b6183b9bfb328377130776525842006ee5f50
Eduardo André <eduardo_andre@id.uff.br>
2023-07-19 15:45:07 -0300

    feat: choice post

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import  express  from "express";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import  cors from "cors";
 import dotenv from "dotenv";
 import dayjs from "dayjs";
+import  date  from "joi";
 
 //url encrypty
 dotenv.config()
@@ -22,27 +23,58 @@ mongoClient.connect()
 
 app.post("/poll",async (req,res)=>{
     const {title,expireAt} = req.body
-    console.log(title,expireAt)
     const enquete ={title:title,expireAt:expireAt}
     if(!title){
         return res.sendStatus(422)
     }
     if(!expireAt){
-        enquete.expireAt = dayjs().add(30,'day').format("YYYY-MM-DD HH:mm")
+        enquete.expireAt = dayjs().add(30,'day').format("YYYY-MM-DD HH:mm:ss")
     }
     try{
+        enquete.expireAt = new Date(enquete.expireAt)
         await db.collection("enquetes").insertOne(enquete)
         return res.sendStatus(201)
     }catch(err){
         return res.status(500).send(err.message)
     }
 })
+
 app.get("/poll",async (req,res)=>{
     try{
         const list_enquetes = await db.collection('enquetes').find().toArray()
         return res.status(200).send(list_enquetes)
     }catch(err){return res.status(500).send(err.message)}
 })
+
+app.post("/choice",async (req,res)=>{
+    const {title,pollId} = req.body
+    const now = new Date()
+    const choice = {title:title,pollId:pollId}
+    if(!title || !pollId){
+        return res.sendStatus(422)
+    }
+    try{
+        const enquete = await db.collection("enquetes").findOne({_id:new ObjectId(pollId)})
+        console.log(enquete.expireAt )
+        if(!enquete){
+            return res.sendStatus(404)
+        }
+        const choice = await db.collection("choices").findOne({title:title})
+        if(choice){
+            return res.sendStatus(409)
+        }
+        console.log(enquete.expireAt)
+        console.log(new Date(enquete.expireAt))
+        console.log(now)
+        if(now.getTime() >new  Date(enquete.expireAt).getTime()){
+            return res.sendStatus(403)
+        }
+        await db.collection("choices").insertOne(choice)
+        res.status(200).send("foi")
+    }catch(err){return res.status(500).send(err.message)}
+})
+
+
 
 app.listen(5000,()=>console.log("api is working"))
 
